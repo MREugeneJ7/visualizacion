fd5722eca8a39b4cc64ea3fa2281973b31d21acf
mreugenej7 <eumadeinchina@gmail.com>
2024-04-02 21:06:38 +0100

    Started Plot creator

---
+++ graph.py
@@ -0,0 +1,12 @@
+import abc
+
+class Graph(metaclass = abc.ABCMeta):
+    
+    def __init__(self, dataframe, y, title) -> None:
+        self._dataframe = dataframe
+        self._y = y
+        self._title = title
+    
+    @abc.abstractmethod 
+    def plot() -> None:
+        pass
