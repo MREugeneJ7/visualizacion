9753d2a5f6223d04665efa0fa88ff897596af36b
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-03 17:21:02 +0100

    Fixed incorrect column name.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Análisis Masivo de Datos
+Análisis Masivo de Datos subject
 Universidad de La Laguna
 Visualization Assignment
 '''
@@ -8,13 +8,14 @@ __authors__ = ["Marcos Barrios", "Eugenio Gonzalez"]
 __date__ = "2024/04/03"
 
 from src.transformers.csvTransformer import CSVTransformer
+from src.transformers.transformer import SourceTransformer
 from src.visualization import Visualization
 from src.visualization import GraphType
 
 def main() -> None:
-    csvTransformer = CSVTransformer('cost-of-living.csv')
+    csvTransformer: SourceTransformer = CSVTransformer('cost-of-living.csv')
     dataframe = csvTransformer.getDataFrame()
-    visualization = Visualization(dataframe.head(), y='x0', groupBy='city')
+    visualization = Visualization(dataframe.head(), y='x1', groupBy='city')
     visualization.setGraph(GraphType.BAR)
     visualization.show()
 
