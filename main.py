5325863ee622200c208b7e2826b02314f1fd4ac2
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2024-04-04 12:03:49 +0100

    Main now graphs top 20 countries by cost of economic food.

--- main.py
+++ main.py
@@ -7,17 +7,32 @@ Visualization Assignment
 __authors__ = ["Marcos Barrios", "Eugenio Gonzalez"]
 __date__ = "2024/04/03"
 
+import pandas as pd
+import numpy as np
+
 from src.visualization import Visualization
 from src.visualization import GraphType
 from src.reader import Reader
 
 def main() -> None:
+    print('Reading csv file.')
     reader = Reader('cost-of-living.csv')
     dataframe = reader.getDataFrame()
-    visualization = Visualization(dataframe.head(), y='x1', groupBy='city',
-                                  title='Test')
+
+    # calculate top 20 countries with the most expensive economic type food
+    topGroups: pd.Series[np.float64] = dataframe.groupby('country')['x1'].max().nlargest(20)
+    dataframeTopCountries = dataframe[dataframe['country'].isin(topGroups.index)]
+    dataframeTopCountriesSorted = dataframeTopCountries.sort_values(by='x1', ascending=False)
+
+    print('Visualising the result.')
+    visualization = Visualization(dataframeTopCountriesSorted, y='x1', groupBy='country',
+                                  title='Coste de comida económica por país')
     visualization.setGraph(GraphType.BAR)
-    visualization.show()
+    def applyExtraConfig(plt):
+        plt.xlabel('País')
+        plt.ylabel('Coste')
+        plt.xticks(rotation=60, ha='right', fontsize=6)
+    visualization.show(post=applyExtraConfig)
 
 if __name__ == '__main__':
     main()
